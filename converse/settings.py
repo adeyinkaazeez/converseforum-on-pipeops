36e53315b34d819b034af73b5ff8b5087b517be1
Azeez Adeyinka <136100206+adeyinkaazeez@users.noreply.github.com>
2025-01-08 14:06:25 +0100

    Update settings.py

--- converse/settings.py
+++ converse/settings.py
@@ -130,18 +130,14 @@ WSGI_APPLICATION = 'converse.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER': 'blog',
-        'PASSWORD': 'xxxxxx',
-	'HOST': '',
-	'PORT':''
-	    
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": '',
+        "PORT": 2085,
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
